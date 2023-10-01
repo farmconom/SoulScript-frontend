7496d980e69989ecacd35fd99a65bd0989b6a9eb
prakasit <prakasit@foobarsystem.com>
2023-10-01 18:48:21 +0700

    add background to dashboard page

--- src/app/page/secure-route/dashboard-route/dashboard/dashboard.component.ts
+++ src/app/page/secure-route/dashboard-route/dashboard/dashboard.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NavigationEnd, NavigationStart, Router } from '@angular/router';
 import { Logger } from 'src/app/utility/logger';
 
 const log = new Logger('collections.page');
@@ -9,6 +10,20 @@ const log = new Logger('collections.page');
   styleUrls: ['./dashboard.component.scss'],
 })
 export class DashboardPage implements OnInit {
+  isPageLoaded = false;
+  isNavigatingToAnotherPage = false;
+
+  constructor(private router: Router) {
+    this.router.events.subscribe((event) => {
+      if (event instanceof NavigationStart) {
+        this.isNavigatingToAnotherPage = true;
+      } else if (event instanceof NavigationEnd) {
+        this.isNavigatingToAnotherPage = false;
+        this.isPageLoaded = true;
+      }
+    });
+  }
+
   ngOnInit(): void {
     log.info('main page');
   }
